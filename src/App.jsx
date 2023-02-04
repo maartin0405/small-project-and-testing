b6f735f6918706973e510aaa042597fed3d5b89d
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-02-04 17:04:20 +0100

    counter :-)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App() {}
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>counter :-) </h1>
+      <p>you clicked button {count} times</p>
+      <button onClick={() => setCount(count + 1)}></button>
+    </>
+  );
+}
 
 export default App;
