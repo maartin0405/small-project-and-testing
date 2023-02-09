221d877ad79daa6d76f881bacd5a886f44e38a28
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-02-09 10:11:51 +0100

    fucked it up or sum

--- components/tictactoe.jsx
+++ components/tictactoe.jsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-  return <button onClick={onSquareClick}>{value}</button>;
+  return <button onClick={onSquareClick}>{value}</button>; // {onSquareClick} runs outside function handeclick by referencing it in props
 }
 
 export function Board() {
-  const [toggleXO, setToggleXO] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+  const [toggleXO, setToggleXO] = useState(true); // toggles between X and O
+  const [squares, setSquares] = useState(Array(9).fill(null)); // defines the square array with buttons with null values
   function handleClick(i) {
+    //this function toggles the right square when clicked based by the index thats sent from handleClick in the array
+    if (squares[i] || CalculateWinner(squares)) {
+      // returns early if square already has a value (X or O)
+      return;
+    }
     const nextSquares = squares.slice();
     if (toggleXO) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
-    }   
-    setSquares(nextSquares);
-    setToggleXO(!toggleXO);
+    }
+    setSquares(nextSquares); // immutable, makes it so the original array is saved and not rewritten
+    setToggleXO(!toggleXO); //switches between X and O
   }
 
   return (
@@ -38,4 +42,25 @@ export function Board() {
       </div>
     </>
   );
+}
+
+function CalculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (lines[i] in lines) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return console.log(squares[a]);
+    }
+    return null;
+  }
 }
